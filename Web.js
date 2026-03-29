5efc4fd833f272ba4bf733c2e295e3376f1f235a
Mike <mukeshpandillapalli2006@gmail.com>
2026-03-29 23:33:54 +0530

    created html,css,js files

---
+++ Web.js
@@ -0,0 +1,17 @@
+
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdown-content");
+    const arrow = document.getElementById("arrow");
+
+    menu.classList.toggle("show");
+    arrow.classList.toggle("rotate");
+}
+
+window.onclick = function(event) {
+    if(!event.target.closest('.dropdown')){
+        document.getElementById("dropdown-content").classList.remove("show");
+
+        document.getElementById("arrow").classList.remove("rotate");
+    }
+}
